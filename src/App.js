e819fa9f8341c7afdfedf0a7703d135ebbb96712
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-07-21 11:12:41 +0200

    added "getTodoList"; updated Calendar slotProps

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import Calendar from "./components/Calendar";
 
 function App({children}) {
+    function getTodoList(date) {
+        console.log('api call get by date', date)
+    }
+
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             {children}
-            <Calendar/>
+            <Calendar getTodoList={getTodoList}/>
         </LocalizationProvider>
     )
 }
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,14 @@
 import {StaticDatePicker} from "@mui/x-date-pickers";
 import dayjs from "dayjs";
 
-function Calendar() {
+function Calendar({getTodoList}) {
     return (
-        <StaticDatePicker defaultValue={dayjs()}/>
+        <StaticDatePicker
+            onChange={(date) => getTodoList(dayjs(date).format("DD.MM.YYYY"))}
+            slotProps={{
+                actionBar: { actions: ['clear'] },
+            }}
+        />
     )
 }
 
