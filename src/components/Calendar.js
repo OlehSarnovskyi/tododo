b481a7fdee7dbee450dbf6900c694050f7c98a9d
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-07-21 11:15:51 +0200

    Added styles for Calendar component

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,14 +1,17 @@
+import './Calendar.css';
 import {StaticDatePicker} from "@mui/x-date-pickers";
 import dayjs from "dayjs";
 
 function Calendar({getTodoList}) {
     return (
-        <StaticDatePicker
-            onChange={(date) => getTodoList(dayjs(date).format("DD.MM.YYYY"))}
-            slotProps={{
-                actionBar: { actions: ['clear'] },
-            }}
-        />
+        <div className="Calendar">
+            <StaticDatePicker
+                onChange={(date) => getTodoList(dayjs(date).format("DD.MM.YYYY"))}
+                slotProps={{
+                    actionBar: { actions: ['clear'] },
+                }}
+            />
+        </div>
     )
 }
 
