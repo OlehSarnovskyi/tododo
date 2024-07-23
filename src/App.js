8e52be9c7ede093a356072784c0fef2edb6a8e2e
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-07-23 09:40:13 +0200

    refactor related to calendar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
-import {LocalizationProvider} from "@mui/x-date-pickers";
-import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import Calendar from "./components/Calendar/Calendar";
 import TodoList from "./components/TodoList/TodoList";
 
-function App({children}) {
+function App() {
     function getTodoList(date) {
         console.log('api call get by date', date)
     }
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            {children}
-            <div className="App">
-                <Calendar getTodoList={getTodoList}/>
-                <TodoList/>
-            </div>
-        </LocalizationProvider>
+        <div className="App">
+            <Calendar getTodoList={getTodoList}/>
+            <TodoList/>
+        </div>
     )
 }
 
--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,17 +1,20 @@
 import './Calendar.css';
-import {StaticDatePicker} from "@mui/x-date-pickers";
+import {LocalizationProvider, StaticDatePicker} from "@mui/x-date-pickers";
 import dayjs from "dayjs";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 
 function Calendar({getTodoList}) {
     return (
-        <div className="Calendar">
-            <StaticDatePicker
-                onChange={(date) => getTodoList(dayjs(date).format("DD.MM.YYYY"))}
-                slotProps={{
-                    actionBar: { actions: ['clear'] },
-                }}
-            />
-        </div>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <div className="Calendar">
+                <StaticDatePicker
+                    onChange={(date) => getTodoList(dayjs(date).format("DD.MM.YYYY"))}
+                    slotProps={{
+                        actionBar: {actions: ['clear']},
+                    }}
+                />
+            </div>
+        </LocalizationProvider>
     )
 }
 
