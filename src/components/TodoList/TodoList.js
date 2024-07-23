8893c53402de9f0aa4cfcef1bb8eef920c535ee0
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-07-23 10:11:15 +0200

    added data structure

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,5 +1,28 @@
 import './TodoList.css';
 
+// from localStorage
+const list = [
+    {
+        id: 'groupId',
+        name: 'groupName',
+        status: 'done',
+        tasks: [
+            {
+                id: 'taskId',
+                text: 'taskText',
+                status: 'done',
+                subtasks: [
+                    {
+                        id: 'subtaskId',
+                        text: 'subtaskText',
+                        status: 'done',
+                    }
+                ]
+            }
+        ]
+    }
+]
+
 function TodoList() {
     return (
         <div className="TodoList">
