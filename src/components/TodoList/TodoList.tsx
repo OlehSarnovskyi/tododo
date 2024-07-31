6a53eed354c7a180466b309302a8cf531a0ebb17
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-08-01 01:14:58 +0200

    implemented initial get list

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -3,34 +3,36 @@ import CreateNew from "./components/CreateNew";
 import Group from "./components/Group/Group";
 import {useState} from "react";
 
+// [{
+//     id: 'groupId',
+//     name: 'groupName',
+//     status: 'done',
+//     tasks: [
+//         {
+//             id: 'taskId',
+//             text: 'taskText',
+//             status: 'done',
+//             subtasks: [
+//                 {
+//                     id: 'subtaskId',
+//                     text: 'subtaskText',
+//                     status: 'done',
+//                 }
+//             ]
+//         },
+//         {
+//             id: 'taskId2',
+//             text: 'taskText 2',
+//             status: 'done',
+//             subtasks: []
+//         }
+//     ]
+// }]
+
 function TodoList() {
     const [isAddingNewGroup, setIsAddingNewGroup] = useState(false)
 
-    const list = [{
-        id: 'groupId',
-        name: 'groupName',
-        status: 'done',
-        tasks: [
-            {
-                id: 'taskId',
-                text: 'taskText',
-                status: 'done',
-                subtasks: [
-                    {
-                        id: 'subtaskId',
-                        text: 'subtaskText',
-                        status: 'done',
-                    }
-                ]
-            },
-            {
-                id: 'taskId2',
-                text: 'taskText 2',
-                status: 'done',
-                subtasks: []
-            }
-        ]
-    }].map(group =>
+    const list = (JSON.parse(localStorage.getItem('tododoList')) || []).map(group => // TODO: create seperate functions to get and set form LS
         <Group key={group.id} group={group}/>
     )
 
