ec5130c55827424013befe31792f11d3b6db1910
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-09-02 15:45:19 +0200

    implemented "/start" command

--- tg-bot/src/bot.mjs
+++ tg-bot/src/bot.mjs
@@ -4,4 +4,14 @@ const bot = new TeleBot(process.env.BOT_ACCESS_TOKEN)
 
 bot.on('text', msg => msg.reply.text(msg.text + 'hahaha'))
 
+bot.on('/start', msg => {
+    const username = `${msg.from.first_name} ${msg.from.last_name} ${msg.from.username} ${msg.from.title}`
+    const welcomeMsg = `
+    Hello ${username}!
+    I'm Tododo bot. I can launch tododo application.
+    Your language code is ${msg.from.language_code}
+    `
+    msg.reply.text(welcomeMsg)
+})
+
 export default bot
