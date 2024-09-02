bcbd07bcac07aa9ba896f1704114dd464c85bbdb
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-09-02 15:18:01 +0200

    fixed tg-bot for deploy

--- tg-bot/src/bot.mjs
+++ tg-bot/src/bot.mjs
@@ -1,18 +1,7 @@
-const TelegramBot = require('node-telegram-bot-api')
-require('dotenv').config()
+import TeleBot from "telebot"
 
-const token = process.env.BOT_ACCESS_TOKEN
-const bot = new TelegramBot(token)
+const bot = new TeleBot(process.env.BOT_ACCESS_TOKEN)
 
-bot.on('message', msg => {
-    const chatId = msg.chat.id
-    const text = msg.text
-
-    if (text === '/launch') {
-        bot.sendMessage(chatId, 'Hello, world!')
-    } else {
-        msg.reply.text(msg.text)
-    }
-})
+bot.on('text', msg => msg.reply.text(msg.text + 'hahaha'))
 
 export default bot
