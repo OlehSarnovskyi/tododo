1b07787678abaaaa36769b08f5fab2894ba5e37b
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-09-02 15:52:00 +0200

    fixed "text" listener

--- tg-bot/src/bot.mjs
+++ tg-bot/src/bot.mjs
@@ -2,16 +2,21 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.BOT_ACCESS_TOKEN)
 
-bot.on('text', msg => msg.reply.text(msg.text + 'hahaha'))
+bot.on('text', msg => {
+    if (!msg.text.startsWith('/')) {
+        msg.reply.text(msg.text + ' hahaha')
+    }
+})
 
 bot.on('/start', msg => {
-    const username = `${msg.from.first_name} ${msg.from.last_name} ${msg.from.username} ${msg.from.title}`
     const welcomeMsg = `
-    Hello ${username}!
+    Hello ${msg.from.first_name}!
     I'm Tododo bot. I can launch tododo application.
-    Your language code is ${msg.from.language_code}
     `
     msg.reply.text(welcomeMsg)
 })
+
+// TODO: support different languages (Your language code is ${msg.from.language_code})
+
 
 export default bot
