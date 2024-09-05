0631b35c4c7828a4d6b843c441eb47e38c2fb74e
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-09-06 01:40:22 +0200

    tg-bot: removed no more needed code

--- tg-bot/src/bot.mjs
+++ tg-bot/src/bot.mjs
@@ -2,9 +2,4 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.BOT_ACCESS_TOKEN)
 
-bot.on('text', msg => {
-    return !msg.text.startsWith('/') ? msg.reply.text(msg.text + ' hahaha') : null
-})
-
-
 export default bot
