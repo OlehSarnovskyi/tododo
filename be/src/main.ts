b259b2206197f42fb680fb91fefdb62910eaaff6
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-10-14 10:34:28 +0200

    be: fixed cors issue

--- be/src/main.ts
+++ be/src/main.ts
@@ -5,7 +5,10 @@ import {AppModule} from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
+  app.enableCors({
+    origin: ['https://tododo-web-app.vercel.app'],
+    credentials: true
+  });
   app.useGlobalPipes(new ValidationPipe({whitelist: true}))
   await app.listen('3000')
 }
