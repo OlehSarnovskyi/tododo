36b552dce27b7e9b4ffa3589b5f2c7e64ad9cc23
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-11-14 15:02:18 +0100

    be: updates main.ts

--- be/src/main.ts
+++ be/src/main.ts
@@ -6,10 +6,10 @@ import {AppModule} from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: ['https://tododo-web-app.vercel.app'],
+    origin: ['https://tododo-web-app.vercel.app', 'http://localhost:3000'],
     credentials: true
   });
   app.useGlobalPipes(new ValidationPipe({whitelist: true}))
-  await app.listen('3000')
+  await app.listen('3001')
 }
 bootstrap()
