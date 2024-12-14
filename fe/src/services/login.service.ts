77cf4f56194fe7d26c9bcd7285c2afb6610cdd35
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-12-14 15:12:24 +0100

    fe: added log

--- fe/src/services/login.service.ts
+++ fe/src/services/login.service.ts
@@ -1,5 +1,8 @@
 import {AxiosInstance} from "axios";
 
 export function login(api: AxiosInstance): Promise<string> {
-    return api.post('users/login', {id: Telegram.WebApp.initDataUnsafe.user?.id}).then(res => res.data)
+    console.(Telegram.WebApp.initDataUnsafe.user);
+    return api.post('users/login', {
+        id: Telegram.WebApp.initDataUnsafe.user?.id
+    }).then(res => res.data)
 }
