4f0f79d5045a88f7b1c931724d4f90bad2f592f6
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2024-12-15 04:05:11 +0100

    fe: fixed some styles for linear loader

--- fe/src/services/login.service.ts
+++ fe/src/services/login.service.ts
@@ -1,11 +1,12 @@
 import {AxiosInstance} from "axios";
 
-export function login(api: AxiosInstance): Promise<string> {
-    return api.post('users/login', {
+export async function login(api: AxiosInstance): Promise<string> {
+    const res = await api.post('users/login', {
         id: Telegram.WebApp.initDataUnsafe.user?.id,
         first_name: Telegram.WebApp.initDataUnsafe.user?.first_name,
         last_name: Telegram.WebApp.initDataUnsafe.user?.last_name,
         username: Telegram.WebApp.initDataUnsafe.user?.username,
         language_code: Telegram.WebApp.initDataUnsafe.user?.language_code
-    }).then(res => res.data)
+    })
+    return res.data
 }
