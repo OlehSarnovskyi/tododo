63e7f06d067e74be4afe4406e2a28d77998d96be
oleh sarnovskyi <oleg.sarnovskiy.98@gmail.com>
2025-01-11 14:26:03 +0100

    fe: added "Loading" text when "isLoading"

--- fe/src/components/TodoList/TodoList.tsx
+++ fe/src/components/TodoList/TodoList.tsx
@@ -6,10 +6,12 @@ import {List} from "@mui/material";
 import {addNewTask, getTasksByUserIdAndDate} from "../../services/tasks.service";
 import {useApiWithSnackbar} from "../../services/api.service";
 import dayjs from "dayjs";
+import {useLoading} from "../../services/loading.service";
 
 function TodoList({date, tasks, setTasksByUserIdAndDate}) {
     const api = useApiWithSnackbar()
     const [isAddingNewTask, setIsAddingNewTask] = useState(false)
+    const { isLoading } = useLoading()
     // TODO: need to refactor
     let tasksTemplates = tasks?.map(task => <Task key={task._id} task={task} date={date} setTasksByUserIdAndDate={setTasksByUserIdAndDate}/>)
 
@@ -36,10 +38,13 @@ function TodoList({date, tasks, setTasksByUserIdAndDate}) {
             <hr/>
             <div className="todo-list-tasks">
                 {tasks.length
-                    ? <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+                    && <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
                         {tasksTemplates}
-                    </List>
-                    : <p className="todo-list-no-tasks">No tasks for {isToday() ? 'today' : 'this day'}</p>}
+                    </List>}
+                {tasks.length === 0 &&
+                    <p className="todo-list-no-tasks">
+                        {isLoading ? 'Loading' : `No tasks for ${isToday() ? 'today' : 'this day'}`}
+                </p>}
             </div>
         </div>
     )
